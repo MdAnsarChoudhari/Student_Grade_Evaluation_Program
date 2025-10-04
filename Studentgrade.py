84451cc7bec3f13b304f02e5892f10b45e23f1a4
Muhammad Ansar M Choudhari <muhammadansarchoudhari@gmail.com>
2025-10-04 08:30:12 +0530

    save in main

---
+++ Studentgrade.py
@@ -0,0 +1,14 @@
+marks=float(input("Enter the marks of the student:"))
+
+if marks>=90:
+    print("Grade S")
+elif marks>=80:
+    print("Grade A")    
+elif marks>=70:
+    print("Grade B")
+elif marks>=60:
+    print("Grade C")
+elif marks>=35:
+    print("Grade D")  
+elif marks<35:
+    print("Fail")

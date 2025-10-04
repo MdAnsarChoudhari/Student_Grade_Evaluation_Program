83b2f77acc62df0307217822889e3f20c08164d3
Muhammad Ansar M Choudhari <muhammadansarchoudhari@gmail.com>
2025-10-04 08:38:38 +0530

    save in grade-average

--- Studentgrade.py
+++ Studentgrade.py
@@ -1,14 +1,25 @@
-marks=float(input("Enter the marks of the student:"))
 
-if marks>=90:
+total = 0
+
+for i in range(1, 6):
+    marks = float(input(f"Enter marks of subject {i}: "))
+    total += marks   
+
+
+avg = total / 5
+
+print("\nAverage Marks:", avg)
+
+
+if avg >= 90:
     print("Grade S")
-elif marks>=80:
-    print("Grade A")    
-elif marks>=70:
+elif avg >= 80:
+    print("Grade A")
+elif avg >= 70:
     print("Grade B")
-elif marks>=60:
+elif avg >= 60:
     print("Grade C")
-elif marks>=35:
-    print("Grade D")  
-elif marks<35:
+elif avg >= 35:
+    print("Grade D")
+else:
     print("Fail")
